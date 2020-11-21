1ad2d863e1282fe471bc9f9b0361258969da63db
canteenms <m.shivam9988@gmail.com>
2020-11-21 08:06:04 +0530

    working background changing and time showing and greetings

--- js/app.js
+++ js/app.js
@@ -1,21 +1,73 @@
-let getHour = document.querySelector('.hour');
-let getMinute = document.querySelector('.minute');
-
-let infoBox = document.querySelector('.info-box');
-document.querySelector('.click').addEventListener('click', function() {
-    infoBox.classList.toggle('h-40');
-    document.querySelector('.quote-box').classList.toggle('disp');
-});
+window.onload = function() {
+    let getHour = document.querySelector('.hour');
+    let getMinute = document.querySelector('.minute');
+    let container = document.querySelector('.container');
 
 
-//show date
-let showDate = function() {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    getHour.innerHTML = hour;
-    getMinute.innerHTML = minute;
-    console.log(hour, minute);
-}
 
-setInterval(showDate, 300);
+
+    let infoBox = document.querySelector('.info-box');
+        document.querySelector('.click').addEventListener('click', function() {
+        infoBox.classList.toggle('h-40');
+        document.querySelector('.quote-box').classList.toggle('disp');
+    });
+
+
+    //show date
+    let showDate = function() { 
+        let date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        if (hour < 10) {
+            getHour.innerHTML = '0' + hour;
+        } else {
+            getHour.innerHTML = hour;
+        }
+        if (minute < 10) {
+            getMinute.innerHTML = '0' + minute;
+        } else {
+            getMinute.innerHTML = minute;
+        }
+        
+        
+        setGreetings(hour, minute);
+    }
+
+    let setGreetings = function(hour, minute) {
+        let timeStamp = document.querySelector('.time-stamp');
+        if(hour >=4 && hour < 12) {
+            timeStamp.innerHTML = "Good Morning";
+            container.style.backgroundImage = "url('../images/morning.jpg')";
+        } else if(hour >= 12 && hour < 17) {
+            timeStamp.innerHTML = "Good Afternoon";
+            container.style.backgroundImage = "url('../images/afternoon.jpg')";
+        } else if(hour >= 17 && hour < 20) {
+            timeStamp.innerHTML = "Good Evening";
+            container.style.backgroundImage = "url('../images/evening.jpg')";
+
+        } else {
+            timeStamp.innerHTML = "Good Night";
+            container.style.backgroundImage = "url('../images/night.jpg')";
+        }
+    };
+    setInterval(showDate, 300);
+};
+
+/* let setGreetings = function(hour, minute) {
+    let timeStamp = document.querySelector('.time-stamp');
+    if(hour >=4 && hour < 12) {
+        console.log('HIi');
+    } else if(hour >= 12 && hour < 17) {
+        console.log('Hello');
+    } else if(hour >= 17 && hour < 20) {
+        console.log('Mello');
+
+    } else {
+        console.log('Jello');
+    }
+}; */
+
+/* timeStamp.innerHTML = "Good Morning";
+        container.style.backgroundImage = "url('../images/morning.jpg')";
+*/
+
